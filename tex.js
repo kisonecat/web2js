aae4e226eb42dc8d7b424f94fed692aecde17ba8
Jim Fowler <fowler@math.osu.edu>
2019-02-11 23:36:25 -0500

    load dump

--- tex.js
+++ tex.js
@@ -7,6 +7,12 @@ var code = new WebAssembly.Module(binary);
 
 var pages = 20;
 var memory = new WebAssembly.Memory({initial: pages, maximum: pages});
+
+var buffer = new Uint8Array( memory.buffer );
+var f = fs.openSync('core.dump','r');
+if (fs.readSync( f, buffer, 0, pages*65536 ) != pages*65536)
+  throw 'Could not load memory dump';
+
 library.setMemory(memory.buffer);
 library.setInput("\n\\input sample");
 
